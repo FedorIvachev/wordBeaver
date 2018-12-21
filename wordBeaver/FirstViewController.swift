7cb3ea33d1c61d2836f927d23fb8ed1bb307a6ec
Rod_Iv <feodor.ivachev@gmail.com>
2018-12-22 01:52:58 +0300

    first commit

---
+++ wordBeaver/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  wordBeaver
+//
+//  Created by Fedor on 21/12/2018.
+//  Copyright © 2018 rodiv. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ wordBeaver/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  wordBeaver
+//
+//  Created by Fedor on 21/12/2018.
+//  Copyright © 2018 rodiv. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
